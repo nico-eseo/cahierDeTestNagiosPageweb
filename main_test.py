784572e98645f6c7f170a0f17959f51796219e25
nico-eseo <95352308+nico-eseo@users.noreply.github.com>
2023-04-18 11:13:47 +0200

    Add files via upload

--- main_test.py
+++ main_test.py
@@ -1,6 +1,12 @@
 #import de packages 
-from flask import Flask
+from flask import Flask, render_template
 import subprocess
+
+class test: 
+    def __init__(self,numero,object,status):
+        self.numero = numero
+        self.object = object
+        self.status = status
 
 #affichage web
 app = Flask(__name__)
@@ -8,7 +14,7 @@ app = Flask(__name__)
 @app.route ('/')
 def hello():
     #envoi de la commande 
-    ipmachine = subprocess.check_output("ip a",shell=True)
+    ipmachine = subprocess.check_output([''],shell=True)
     web = '<html><body><h1>site test pour le test</h1><pre>{}</pre></body></html>'.format(ipmachine.decode("utf-8"))
     return web
 
