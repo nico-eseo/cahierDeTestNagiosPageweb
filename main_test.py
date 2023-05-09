82f9c91189c9d7cca452b3e39b0425f85b64d51e
nico-eseo <95352308+nico-eseo@users.noreply.github.com>
2023-05-09 09:54:55 +0200

    Add files via upload

--- main_test.py
+++ main_test.py
@@ -10,21 +10,40 @@ class Test:
 
 #affichage web
 app = Flask(__name__)
+file = open('check.txt',mode = 'r', encoding='utf-8-sig')
+lines = file.readlines()
 
 @app.route ('/')
 def index():
     #ouverture du fichier check.txt
-    file = open('check.txt',mode = 'r', encoding='utf-8-sig')
-    lines = file.readline()
+    
     dict = {}
     tests = []
+    line_split = []
     #envoi de la commande 
     for line in lines:
-        line = line.split('-')
-            
-        tests.append(Test(line,'test','test'))
-    file.close()
+        
+        line_split = line.split('-')
+        line_split = [j.strip() for j in line_split]
+        length = len(line_split)
+        match length:
+            case 1 :
+                dict['resultat attendue'] = line_split[0]
+            case 2 :
+                dict['resultat attendue'] = line_split[0]
+                dict['resultat '] = line_split[1]
+            case 3 :
+                dict['resultat attendue'] = line_split[0]
+                dict['resultat '] = line_split[1]
+                dict['detaille'] = line_split[2]
+            case 4 :
+                dict['resultat attendue'] = line_split[0]
+                dict['resultat '] = line_split[1]
+                dict['detaille'] = line_split[2]
+        
+        tests.append(Test(dict['resultat attendue'],dict['resultat '],dict['detaille']))
+    
     return render_template('index.html',tests=tests)
-
+file.close()
 if __name__ == '__main__':
     app.run(debug=True) 
