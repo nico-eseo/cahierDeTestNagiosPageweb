1b9839eeb411e6a7244b7fa126a204c29cbbb5f4
nico-eseo <95352308+nico-eseo@users.noreply.github.com>
2023-06-14 13:44:41 +0200

    Add files via upload

--- main_test.py
+++ main_test.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template
 import subprocess
 
 class Test: 
-    def __init__(self,numero,object,status):
+    def __init__(self,numero,object,status,validation):
         self.numero = numero
         self.object = object
         self.status = status
+        self.validation = validation
 
 #affichage web
 app = Flask(__name__)
@@ -32,16 +33,44 @@ def index():
             case 2 :
                 dict['resultat attendue'] = line_split[0]
                 dict['resultat '] = line_split[1]
+                if 'OK' in line_split[1]:
+                    dict['validation'] = "validé"
+                elif 'CRITICAL' in line_split[1]:
+                    dict['validation'] = "non validé"
+                elif 'Error' in line_split[1]:
+                    dict['validation'] = "non validé"
+                else :
+                    dict['validation'] = "non concluant"
+
             case 3 :
                 dict['resultat attendue'] = line_split[0]
                 dict['resultat '] = line_split[1]
+                if 'OK' in line_split[1]:
+                    dict['validation'] = "validé"
+                elif 'CRITICAL' in line_split[1]:
+                    dict['validation'] = "non validé"
+                elif 'Error' in line_split[1]:
+                    dict['validation'] = "non validé"
+                else :
+                    dict['validation'] = "non concluant"
+
                 dict['detaille'] = line_split[2]
             case 4 :
                 dict['resultat attendue'] = line_split[0]
                 dict['resultat '] = line_split[1]
-                dict['detaille'] = line_split[2]
+                
+                if 'OK' in line_split[1]:
+                    dict['validation'] = "validé"
+                elif 'CRITICAL' in line_split[1]:
+                    dict['validation'] = "non validé"
+                elif 'Error' in line_split[1]:
+                    dict['validation'] = "non validé"
+                else :
+                    dict['validation'] = "non concluant"
+                
+                dict['detaille'] = line_split[2] + line_split[3]
         
-        tests.append(Test(dict['resultat attendue'],dict['resultat '],dict['detaille']))
+        tests.append(Test(dict['resultat attendue'],dict['resultat '],dict['detaille'],dict['validation']))
     
     return render_template('index.html',tests=tests)
 file.close()
